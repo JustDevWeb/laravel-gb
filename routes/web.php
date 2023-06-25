dfa1fd415e81bebb9b5432fe7237878ef422115c
JustDevWeb <zm91672@gmail.com>
2023-06-25 10:53:55 +0500

    dev: add routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', function () {
+    return "<h1>Welcome to blog</h1>";
+});
+
+Route::get('/about', function () {
+    return "<h1>About the project</h1>";
+});
+
+Route::get('/news', function () {
+    return "<h1>News</h1>";
+});
+
