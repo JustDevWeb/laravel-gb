9fab01b568daa3afd277ef980e4929d3654ef20c
JustDevWeb <zm91672@gmail.com>
2023-07-19 09:57:11 +0500

    change syntax in blade template

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,12 +1,11 @@
 <h2>News List</h2>
-<?php foreach($newsList as $news): ?>
+@foreach($newsList as $news)
 <div>
-    <h4><a href="<?= route("news.show",$news["id"]) ?>"><?=$news["title"]?></a></h4>
+    <h4><a href="{{ route("news.show",$news["id"]) }}">{{$news["title"]}}</a></h4>
     <br/>
-    <img src="<?=$news["image"]?>" />
-    <p><em><?=$news["author"]?> &nbsp; (<?=$news["created_at"]?>)</em></p>
-    <p><a href="<?= route("category.show",$news["category"]) ?>"><?=$news["category"]?></a></p>
-    <p><?=$news["description"]?></p>
-
+    <img src="{{$news["image"]}}" />
+    <p><em>{{$news["author"]}}&nbsp ({{$news["created_at"]}})</em></p>
+    <p><a href="{{route("category.show",$news["category"])}}">{{$news["category"]}}</a></p>
+    <p>{{$news["description"]}}</p>
 </div>
-<?php endforeach; ?>
+@endforeach
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,8 +1,8 @@
-<h2><?=$news["id"]?></h2>
+<h2>{{$news["id"]}}</h2>
 
 <div>
-    <img src="<?=$news["image"]?>" />
-    <p><em><?=$news["author"]?> &nbsp; (<?=$news["created_at"]?>)</em></p>
-    <p><a href="<?= route("category.show",$news["category"]) ?>"><?=$news["category"]?></a></p>
-    <p><?=$news["description"]?></p>
+    <img src="{{$news["image"]}}" />
+    <p><em>{{$news["author"]}} &nbsp; ({{$news["created_at"]}})</em></p>
+    <p><a href="{{route("category.show",$news["category"])}}">{{$news["category"]}}</a></p>
+    <p>{{$news["description"]}}></p>
 </div>
